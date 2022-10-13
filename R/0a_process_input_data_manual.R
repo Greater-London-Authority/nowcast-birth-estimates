2f36c2da6e751e91e8339d85c13146d7a044da5a
Ben Corr <bjcorr606@gmail.com>
2022-10-13 08:51:40 +0100

    change location of lookup files to /lookups

--- R/0a_process_input_data_manual.R
+++ R/0a_process_input_data_manual.R
@@ -1,7 +1,3 @@
-source("R/data_prep_scripts/create_lsoa_lad_lookup.R")
-source("R/data_prep_scripts/create_lad_rgn_lookup.R")
-source("R/data_prep_scripts/create_lad_ctry_lookup.R")
-source("R/data_prep_scripts/create_lad_itl_lookup.R")
 source("R/data_prep_scripts/process_cy_lsoa_births.R")
 source("R/data_prep_scripts/process_mye_births.R")
 source("R/data_prep_scripts/combine_and_aggregate_births.R")
--- R/data_prep_scripts/combine_and_aggregate_births.R
+++ R/data_prep_scripts/combine_and_aggregate_births.R
@@ -1,10 +1,12 @@
+library(tidyverse)
+
 source("R/functions/aggregate_to_region.R")
 
 fpath <- list(births_cy_lad = "data/intermediate/births_cy_lad.rds",
               births_my_lad = "data/intermediate/births_my_lad.rds",
-              lookup_lad_rgn = "data/intermediate/lookup_lad_rgn.rds",
-              lookup_lad_itl = "data/intermediate/lookup_lad_itl.rds",
-              lookup_lad_ctry = "data/intermediate/lookup_lad_ctry.rds",
+              lookup_lad_rgn = "lookups/lookup_lad_rgn.rds",
+              lookup_lad_itl = "lookups/lookup_lad_itl.rds",
+              lookup_lad_ctry = "lookups/lookup_lad_ctry.rds",
               births_actual = "data/processed/births_actual.rds")
 
 births_lad <- bind_rows(readRDS(fpath$births_cy_lad),
