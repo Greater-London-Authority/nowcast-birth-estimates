129f6ea44f1c5763879bcfa45034683c27dabdb4
Marta Lapsley <marta.lapsley@london.gov.uk>
2024-02-06 11:37:51 +0000

    finished updating datastore script

--- R/6_update_datastore.R
+++ R/6_update_datastore.R
@@ -62,37 +62,35 @@ datastore_resources_descriptions<-
        plots = "Selected plots of actual and predicted recent births for English local authorities, regions, and ITL 2 subregions. Plots are labelled by their geographic code - a csv lookup of codes and names is included in the zip file.",
        ratios = "File containing: past birth estimates, modelled counts of patients age 0, actual and modelled ratios of births to patient counts. Data for English local authorities, regions, and ITL2 subregions.")
 
-#names(datastore_resources_list)<-seq(1:length(datastore_resources_list))
-#names(datastore_resources_descriptions)<-seq(1:length(datastore_resources_descriptions))
-
 
 # The following algorithm checks if there are any resources associated with this dataset, and uploads all the ones in
 # datastore_resources_list if there aren't any.
 
 # If there are already resources associated with the dataset which is being modified then where a new resource has
-# the same name as an existing resource it will replace it. Otherwise the new resources will be added to those
+# the same name as an existing resource it will replace it. Otherwise the new resources will be added alongside those
 # that are already there.
 
 if (!"resource_id" %in% colnames(lds_meta_dataset(slug=page_slug, my_api_key))) {
 
-  mapply(function(x, y) lds_add_resource(file_path=x,
+  mapply(function(x, y) lds_add_resource(file_path = x,
                                          description = y,
                                          slug=page_slug,
                                          my_api_key),
-         datastore_resources_list,  # names from first
+         datastore_resources_list,
          datastore_resources_descriptions)
-
-
-  # map(datastore_resources_list,
-  #     ~lds_add_resource(
-  #       file_path=.x,
-  #       slug=page_slug,
-  #       my_api_key
-  #     ))
 
 } else {
 
-  new_resources_names <- map(datastore_resources_list, basename)
+  datastore_resources_descriptions<-
+    bind_rows(datastore_resources_descriptions) %>%
+    gather(list_item, description)
+
+  datastore_resources_list<-
+    bind_rows(datastore_resources_list) %>%
+    gather(list_item, filepath) %>%
+    mutate(name = basename(filepath)) %>%
+    left_join(datastore_resources_descriptions, by="list_item") %>%
+    select(-list_item)
 
   current_resources_names<-
     select(as_tibble(lds_meta_dataset(slug=page_slug, my_api_key)),
@@ -100,17 +98,18 @@ if (!"resource_id" %in% colnames(lds_meta_dataset(slug=page_slug, my_api_key))) 
            resource_id)
 
   datastore_resources_list<-
-    full_join(datastore_resources_list,
-              current_resources_names, by=c("name2"="resource_title2")) %>%
-    mutate(name_for_resource=basename(name))
+    left_join(datastore_resources_list,
+              current_resources_names, by=c("name"="resource_title"))
 
   for (i in 1:nrow(datastore_resources_list)) {
 
     if (is.na(datastore_resources_list$resource_id[i])) {
 
       lds_add_resource(
-        file_path=datastore_resources_list$name[i],
-        slug=page_slug,
+        file_path = datastore_resources_list$filepath[i],
+        res_title = datastore_resources_list$name[i],
+        description = datastore_resources_list$description[i],
+        slug = page_slug,
         my_api_key
       )
     }
@@ -118,10 +117,11 @@ if (!"resource_id" %in% colnames(lds_meta_dataset(slug=page_slug, my_api_key))) 
     else {
 
       lds_replace_resource(
-        file_path=datastore_resources_list$name[i],
+        file_path=datastore_resources_list$filepath[i],
         slug=page_slug,
         res_id=datastore_resources_list$resource_id[i],
-        res_title=datastore_resources_list$name_for_resource[i],
+        res_title=datastore_resources_list$name[i],
+        description = datastore_resources_list$description[i],
         api_key=my_api_key
       )
     }
